ee99e5eb6e70afaf7cda59f693ec053e30ff81a9
Neapolicy <matthewlin559@gmail.com>
2024-03-11 20:54:10 -0400

    placeholder audio player, unknown if it will work, also added some audio

--- core/src/Inputs/KeyboardInputs.java
+++ core/src/Inputs/KeyboardInputs.java
@@ -1,4 +1,31 @@
 package Inputs;
 
-public class KeyboardInputs {
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class KeyboardInputs implements MouseListener { //use motion-listener if necessary
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
