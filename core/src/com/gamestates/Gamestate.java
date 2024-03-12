94dac04db4dbc1ce54e55e3c9f70b404d72a1093
Neapolicy <matthewlin559@gmail.com>
2024-03-11 20:30:23 -0400

    added stuff

---
+++ core/src/com/gamestates/Gamestate.java
@@ -0,0 +1,8 @@
+package com.gamestates;
+
+public enum Gamestate {
+
+    MENU, PLAYING, ENDSCREEN;
+
+    public static Gamestate state = Gamestate.MENU;
+}
--- desktop/src/com/gamestates/DesktopLauncher.java
+++ desktop/src/com/gamestates/DesktopLauncher.java
@@ -1,8 +1,7 @@
-package com.mygdx.game;
+package com.gamestates;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher { //desktop, don't touch unless you want to change fps, most of your work is done in core, assets is where sprites go
