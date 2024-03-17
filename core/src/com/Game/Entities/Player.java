9704f1f13607cf8c410c68b5c4b235b12d89d793
Neapolicy <matthewlin559@gmail.com>
2024-03-16 22:14:18 -0400

    player now turns left and right

--- core/src/com/Game/Entities/Player.java
+++ core/src/com/Game/Entities/Player.java
@@ -6,47 +6,57 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 
-public class Player extends Entity{
+public class Player extends Entity {
     private Animator animator = new Animator();
     private boolean isRunning = false;
     private boolean isIdle = false;
+    private boolean left = false;
+
     public Player(int speed, Texture text, int x, int y, Playing screen) {
         super(speed, text, x, y, screen);
         sprite.setScale(.01f);
-        sprite.setSize((float) (.1 * sprite.getWidth()), (float) (.1 * sprite.getHeight()));
-        animator.changeColnRows(5, 1);
-        animator.createAnimation(new Texture("Sprites/player_idle.png"));
     }
+
     public void update(float deltatime) {
+        // Check for movement keys
+        boolean isMoving = false; // Flag to track movement
+
         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
             position.y += speed * deltatime;
-            playerRun();
-            isIdle = false;
+            isMoving = true;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.A)) {
             position.x -= speed * deltatime;
-            playerRun();
-            isIdle = false;
+            isMoving = true;
+            left = true;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.S)) {
             position.y -= speed * deltatime;
-            playerRun();
-            isIdle = false;
+            isMoving = true;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
             position.x += speed * deltatime;
-            playerRun();
-            isIdle = false;
+            isMoving = true;
+            left = false;
         }
-        else{
+
+        // Update animation states based on movement
+        if (isMoving) {
+            playerRun();
+            isIdle = false; // Set idle flag to false
+        } else {
             playerIdle();
-            isRunning = false;
+            isRunning = false; // Set running flag to false
         }
     }
+
     public void playerRun() {
         if (!isRunning) {
             animator.changeColnRows(6, 1);
-            animator.createAnimation(new Texture("Sprites/player_run.png"));
+            if (!left) animator.createAnimation(new Texture("Sprites/player_run.png"));
+            else {
+                animator.createAnimation(new Texture("Sprites/player_run_left.png"));
+            }
             isRunning = true; // Set the flag to true
         }
     }
@@ -54,17 +64,22 @@ public class Player extends Entity{
     public void playerIdle() {
         if (!isIdle) {
             animator.changeColnRows(5, 1);
-            animator.createAnimation(new Texture("Sprites/player_idle.png"));
+            if (!left) animator.createAnimation(new Texture("Sprites/player_idle.png"));
+            else {
+                animator.createAnimation(new Texture("Sprites/player_idle_left.png"));
+            }
             isIdle = true; // Set the flag to true
         }
     }
-    public void draw(){
+
+    public void draw() {
         update(Gdx.graphics.getDeltaTime());
         animator.render((int) position.x, (int) position.y);
         sprite.setPosition(position.x, position.y);
         sprite.draw(screen.getGame().batch);
     }
-    public Animator getAnimator(){
+
+    public Animator getAnimator() {
         return animator;
     }
 }
