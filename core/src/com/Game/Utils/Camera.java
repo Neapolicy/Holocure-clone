c8083abd3333e1d92acd684b58758abe8612fd6d
Neapolicy <matthewlin559@gmail.com>
2024-03-18 23:10:55 -0400

    IT WORKS ASDASDASDASD

---
+++ core/src/com/Game/Utils/Camera.java
@@ -0,0 +1,4 @@
+package com.Game.Utils;
+
+public class Camera { //https://www.youtube.com/watch?v=KDQ_Juy8sFs vid might be helpful idk lmao this guy uses vector 3 so idrk but he is making a 2d game
+}
