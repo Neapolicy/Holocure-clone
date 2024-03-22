ea754885ae78a22518be51472262d4cbb0538c09
neapolicy <matthewlin559@gmail.com>
2024-03-22 11:46:53 -0400

    trying to figure out how to solve this issue

--- core/src/com/Game/Objects/Weapon.java
+++ core/src/com/Game/Objects/Weapon.java
@@ -3,6 +3,8 @@ package com.Game.Objects;
 import com.Game.Entities.Player;
 import com.Game.Utils.Animator;
 import com.Game.myGdxGame;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
@@ -12,12 +14,14 @@ public class Weapon {
     public Sprite sprite;
     public Vector2 position;
     public Animator animator = new Animator();
+    public Sound sound;
     public myGdxGame game;
-    public Weapon(Texture text, int x, int y, myGdxGame game){
+    public Weapon(Texture text, int x, int y, String fileLink, myGdxGame game){
         this.game = game;
         texture = text;
         sprite = new Sprite(texture);
         position = new Vector2(x, y);
+        sound = Gdx.audio.newSound(Gdx.files.internal(fileLink));
     }
 
     public void attack(float time, int cd, Player player){
