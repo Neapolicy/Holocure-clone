094cd2515b632fa6bfca3640cb14acaa4d8bd20f
Neapolicy <matthewlin559@gmail.com>
2024-03-24 23:28:27 -0400

    cleaned up player class

---
+++ core/src/com/Game/Objects/Axe.java
@@ -0,0 +1,11 @@
+package com.Game.Objects;
+
+import com.Game.Entities.Entity;
+import com.Game.myGdxGame;
+import com.badlogic.gdx.graphics.Texture;
+
+public class Axe extends Entity {
+    public Axe(int speed, Texture text, int x, int y, myGdxGame screen) {
+        super(speed, text, x, y, screen);
+    }
+}
---
+++ core/src/com/Game/Objects/Gun.java
@@ -0,0 +1,10 @@
+package com.Game.Objects;
+
+import com.Game.Entities.Player;
+import com.Game.myGdxGame;
+
+public class Gun extends Weapon{
+    public Gun(int x, int y, int damage, Player player, myGdxGame game) {
+        super(x, y, damage, player, game);
+    }
+}
