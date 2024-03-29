614af99361486e17e520dcc9ca5584e0175746a8
Neapolicy <matthewlin559@gmail.com>
2024-03-28 21:48:22 -0400

    enemy follows player, should try and implement pathfinding soon

--- core/src/com/Game/Entities/Enemy.java
+++ core/src/com/Game/Entities/Enemy.java
@@ -2,7 +2,6 @@ package com.Game.Entities;
 
 import com.Game.Utils.Constants;
 import com.Game.myGdxGame;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
